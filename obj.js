a39d241c162a3ff56991f7053d4a8ecda71bb2d3
Atajan <chessmaster_87@mail.ru>
2021-11-27 14:06:11 +0500

    added constructor Product

--- obj.js
+++ obj.js
@@ -1,5 +1,8 @@
+'use strict';
 //objects home-work
-//first
+
+
+//first 1
 
 /**
  * Number to object
@@ -23,3 +26,29 @@ function run() {
 	const newObj = createObj(+prompt("Enter number between 0-999"));
 	console.log(newObj);
 }
+
+//second 1.1
+//es5
+function Product(name, price) {
+	this.name = name;
+	this.price = price;
+}
+Product.prototype.make25PercentDiscount = function () {
+	this.price -= Math.round((this.price / 4) * 100) / 100;
+}
+
+const prod1 = new Product('apple', 406);
+const prod2 = new Product('cherry', 502);
+
+//es6
+class ProductNew {
+	constructor(name, price) {
+		this.name = name;
+		this.price = price;
+	}
+	make25PercentDiscount() {
+		this.price -= Math.round((this.price / 4) * 100) / 100;
+	}
+}
+const newProd1 = new ProductNew('chocolate', 620);
+const newProd2 = new ProductNew('cake', 213);
